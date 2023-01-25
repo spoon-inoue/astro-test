0cee101c1f52489cdafabc55efefbd7ac2c662ad
spoon-inoue <h-inoue@studio-spoon.co.jp>
2023-01-25 18:40:17 +0900

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,9 @@
 import { defineConfig } from 'astro/config'
 import prefetch from '@astrojs/prefetch'
 
-// https://astro.build/config
-// export default defineConfig = ({ mode }) => {
-// 	console.log('⚓ ' + mode)
-// 	return {
-// 		base: mode === 'astro dev' ? '/' : '/astro-test/',
-// 		integrations: [
-// 			prefetch({
-// 				selector: "a[href^='/']",
-// 			}),
-// 		],
-// 	}
-// }
-
 export default defineConfig({
-	site: 'https://spoon-inoue.github.io/astro-test/',
+	site: 'https://spoon-inoue.github.io',
+	// base: '/astro-test',
 	integrations: [
 		prefetch({
 			selector: "a[href^='/']",
