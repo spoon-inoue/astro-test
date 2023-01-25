bb27cddc467dcde11ac5283ca17845fa29204e3a
spoon-inoue <h-inoue@studio-spoon.co.jp>
2023-01-25 18:09:50 +0900

    init

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config'
+import prefetch from '@astrojs/prefetch'
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		prefetch({
+			selector: "a[href^='/']",
+		}),
+	],
+})
---
+++ src/scripts/cards.ts
@@ -0,0 +1,26 @@
+export const cards: { href: string; title: string; body: string; bgColor?: string }[] = [
+	{
+		href: 'https://docs.astro.build/',
+		title: 'Documentation',
+		body: 'Learn how Astro works and explore the official API docs.',
+		bgColor: '#aaf',
+	},
+	{
+		href: 'https://astro.build/integrations/',
+		title: 'Integrations',
+		body: 'Supercharge your project with new frameworks and libraries.',
+		bgColor: '#ffa',
+	},
+	{
+		href: 'https://astro.build/themes/',
+		title: 'Themes',
+		body: 'Explore a galaxy of community-built starter themes.',
+		bgColor: '#aff',
+	},
+	{
+		href: 'https://astro.build/chat/',
+		title: 'Community',
+		body: 'Come say hi to our amazing Discord community. ❤️',
+		bgColor: '#faa',
+	},
+]
